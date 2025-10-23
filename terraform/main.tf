1af2e49c3a9ed3fa994cb7feb793fcb3469c419c
dobigoth <circa1902@yahoo.com>
2025-10-23 20:17:27 +0800

    Allow public static website4

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,21 +12,6 @@ resource "aws_s3_bucket_website_configuration" "static_website_config" {
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "ownership_dobigoth" {
-  bucket = aws_s3_bucket.static_sites_dobigoth.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "acl_dobigoth" {
-  #depends_on = [aws_s3_bucket_ownership_controls.example]
-  bucket = aws_s3_bucket.static_sites_dobigoth.id
-  acl    = "private"
-
-}
-
-
 resource "aws_s3_bucket_public_access_block" "access_dobigoth" {
   bucket = aws_s3_bucket.static_sites_dobigoth.id
 
@@ -53,26 +38,44 @@ resource "aws_s3_bucket_policy" "static_site_policy" {
   depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
 }
 
-resource "aws_s3_bucket_versioning" "versioning_example" {
-  bucket = aws_s3_bucket.static_sites_dobigoth.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_ownership_controls" "ownership_dobigoth" {
+#   bucket = aws_s3_bucket.static_sites_dobigoth.id
+#   rule {
+#     object_ownership = "BucketOwnerPreferred"
+#   }
+# }
 
-resource "aws_kms_key" "mykey" {
-  description             = "This key is used to encrypt bucket objects"
-  enable_key_rotation     = true
-  deletion_window_in_days = 7
-}
+# resource "aws_s3_bucket_acl" "acl_dobigoth" {
+#   #depends_on = [aws_s3_bucket_ownership_controls.example]
+#   bucket = aws_s3_bucket.static_sites_dobigoth.id
+#   acl    = "private"
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
-  bucket = aws_s3_bucket.static_sites_dobigoth.id
+# }
 
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.mykey.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
-}
+
+
+# }
+
+# resource "aws_s3_bucket_versioning" "versioning_example" {
+#   bucket = aws_s3_bucket.static_sites_dobigoth.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
+
+# resource "aws_kms_key" "mykey" {
+#   description             = "This key is used to encrypt bucket objects"
+#   enable_key_rotation     = true
+#   deletion_window_in_days = 7
+# }
+
+# resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+#   bucket = aws_s3_bucket.static_sites_dobigoth.id
+
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       kms_master_key_id = aws_kms_key.mykey.arn
+#       sse_algorithm     = "aws:kms"
+#     }
+#   }
+# }
