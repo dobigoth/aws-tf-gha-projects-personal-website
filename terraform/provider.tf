ccb6f5e8c350efded597e59aa82555f2a2fa7b26
dobigoth <circa1902@yahoo.com>
2025-10-23 15:42:22 +0800

    list s3 bucket

---
+++ terraform/provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-tayocash-backend"
+    region  = "ap-southeast-1"
+    key     = "github-actions/terraform.tfstate"
+    encrypt = true
+    dynamodb_table = "tf_resouces-gha-lock"
+  }
+}
