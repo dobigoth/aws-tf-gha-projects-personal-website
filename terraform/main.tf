6ab4732f3c88e8c07e1b03cd33614b95b7afe8d4
dobigoth <circa1902@yahoo.com>
2025-10-23 20:11:19 +0800

    Allow public static website3

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,14 +27,14 @@ resource "aws_s3_bucket_acl" "acl_dobigoth" {
 }
 
 
-# resource "aws_s3_bucket_public_access_block" "access_dobigoth" {
-#   bucket = aws_s3_bucket.static_sites_dobigoth.id
+resource "aws_s3_bucket_public_access_block" "access_dobigoth" {
+  bucket = aws_s3_bucket.static_sites_dobigoth.id
 
-#   block_public_acls       = true
-#   block_public_policy     = true
-#   ignore_public_acls      = true
-#   restrict_public_buckets = true
-# }
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 
 resource "aws_s3_bucket_policy" "static_site_policy" {
   bucket = aws_s3_bucket.static_sites_dobigoth.id
