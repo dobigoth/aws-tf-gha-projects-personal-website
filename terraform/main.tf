74e14e21fcc934fcfbcabfcad84ff8ab74340664
dobigoth <circa1902@yahoo.com>
2025-10-23 20:04:53 +0800

    Allow public static website

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,13 +20,30 @@ resource "aws_s3_bucket_acl" "acl_dobigoth" {
 }
 
 
-resource "aws_s3_bucket_public_access_block" "access_dobigoth" {
-  bucket = aws_s3_bucket.static_sites_dobigoth.id
+# resource "aws_s3_bucket_public_access_block" "access_dobigoth" {
+#   bucket = aws_s3_bucket.static_sites_dobigoth.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
+
+resource "aws_s3_bucket_policy" "static_site_policy" {
+  bucket = aws_s3_bucket.static_site.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+
+  depends_on = [ aws_s3_bucket_public_access_block.static_site_access ]
 }
 
 resource "aws_s3_bucket_versioning" "versioning_example" {
